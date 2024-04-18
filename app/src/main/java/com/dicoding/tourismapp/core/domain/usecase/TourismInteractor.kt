41d89eee246a9061ba850dd8080486f65af88383
Karina Aulia Primanti Putri <karinauliap@gmail.com>
2024-04-18 19:25:09 +0700

    [learn] studi kasus coroutine flow pada aplikasi

--- app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismInteractor.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismInteractor.kt
@@ -1,14 +1,15 @@
 package com.dicoding.tourismapp.core.domain.usecase
 
+import com.dicoding.tourismapp.core.data.Resource
 import com.dicoding.tourismapp.core.domain.model.Tourism
 import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
-import io.reactivex.Flowable
+import kotlinx.coroutines.flow.Flow
 
 class TourismInteractor(private val tourismRepository: ITourismRepository): TourismUseCase {
 
-    override fun getAllTourism() = tourismRepository.getAllTourism()
+    override fun getAllTourism(): Flow<Resource<List<Tourism>>> = tourismRepository.getAllTourism()
 
-    override fun getFavoriteTourism(): Flowable<List<Tourism>> = tourismRepository.getFavoriteTourism()
+    override fun getFavoriteTourism(): Flow<List<Tourism>> = tourismRepository.getFavoriteTourism()
 
     override fun setFavoriteTourism(tourism: Tourism, state: Boolean) = tourismRepository.setFavoriteTourism(tourism, state)
 }
