88c62dee5e51cca71c149659232c898e9918e684
Karina Aulia Primanti Putri <karinauliap@gmail.com>
2024-04-18 19:12:25 +0700

    [learn] studi kasus reactiveX (RxJava) pada aplikasi

--- app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
@@ -7,6 +7,11 @@ import com.dicoding.tourismapp.core.data.source.remote.network.ApiResponse
 import com.dicoding.tourismapp.core.data.source.remote.network.ApiService
 import com.dicoding.tourismapp.core.data.source.remote.response.ListTourismResponse
 import com.dicoding.tourismapp.core.data.source.remote.response.TourismResponse
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,28 +27,25 @@ class RemoteDataSource private constructor(private val apiService: ApiService) {
             }
     }
 
-    fun getAllTourism(): LiveData<ApiResponse<List<TourismResponse>>> {
-        val resultData = MutableLiveData<ApiResponse<List<TourismResponse>>>()
+    fun getAllTourism(): Flowable<ApiResponse<List<TourismResponse>>> {
+        val resultData = PublishSubject.create<ApiResponse<List<TourismResponse>>>()
 
         //get data from remote api
         val client = apiService.getList()
 
-        client.enqueue(object : Callback<ListTourismResponse> {
-            override fun onResponse(
-                call: Call<ListTourismResponse>,
-                response: Response<ListTourismResponse>
-            ) {
-                val dataArray = response.body()?.places
-                resultData.value = if (dataArray != null) ApiResponse.Success(dataArray) else ApiResponse.Empty
-            }
+        client
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .take(1)
+            .subscribe ({ response ->
+                val dataArray = response.places
+                resultData.onNext(if (dataArray.isNotEmpty()) ApiResponse.Success(dataArray) else ApiResponse.Empty)
+            }, { error ->
+                resultData.onNext(ApiResponse.Error(error.message.toString()))
+                Log.e("RemoteDataSource", error.toString())
+            })
 
-            override fun onFailure(call: Call<ListTourismResponse>, t: Throwable) {
-                resultData.value = ApiResponse.Error(t.message.toString())
-                Log.e("RemoteDataSource", t.message.toString())
-            }
-        })
-
-        return resultData
+        return resultData.toFlowable(BackpressureStrategy.BUFFER)
     }
 }
 
--- app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismInteractor.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismInteractor.kt
@@ -2,12 +2,13 @@ package com.dicoding.tourismapp.core.domain.usecase
 
 import com.dicoding.tourismapp.core.domain.model.Tourism
 import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
+import io.reactivex.Flowable
 
 class TourismInteractor(private val tourismRepository: ITourismRepository): TourismUseCase {
 
     override fun getAllTourism() = tourismRepository.getAllTourism()
 
-    override fun getFavoriteTourism() = tourismRepository.getFavoriteTourism()
+    override fun getFavoriteTourism(): Flowable<List<Tourism>> = tourismRepository.getFavoriteTourism()
 
     override fun setFavoriteTourism(tourism: Tourism, state: Boolean) = tourismRepository.setFavoriteTourism(tourism, state)
 }
